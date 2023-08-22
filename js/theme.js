15ea103a2bf429554d5743b510066bf404014da3
Zubair Khan <kzubair129@gmail.com>
2023-08-22 23:54:33 +0530

    Dark light mode complete

---
+++ js/theme.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const  /** {NodeElement} */ $HTML= document.documentElement;
+let /**{Boolean} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if(sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+
+else {
+    $HTML.dataset.theme= isDark ? "dark" : "light";
+}
+
+const changeTheme = function () {
+    console.log("click");
+    isDark = sessionStorage.getItem("theme");
+    sessionStorage.setItem("theme", isDark === "light"  ? "dark": "light");
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+
+}
+
+window.addEventListener("load", ()=>{
+    const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-toggler]");
+    $themeBtn.addEventListener("click", changeTheme);
+});
+
