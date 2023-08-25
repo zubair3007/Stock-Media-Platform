02e45744d9d36046740e385c8f7e0de4eeb873eb
Zubair Khan <kzubair129@gmail.com>
2023-08-25 20:06:37 +0530

    Navigation structure and style completed

--- js/global.js
+++ js/global.js
@@ -8,6 +8,9 @@
  * Imports
  */
 import { ripple } from "./utils/ripple.js";
+import { addEventOnElements } from "./utils/event.js";
+
+
 
 
 
@@ -27,6 +30,20 @@ window.addEventListener("scroll", ()=> {
 
 const /** {NodeList} */ $rippleElems = document.querySelectorAll("[data-ripple]"); 
 $rippleElems.forEach($rippleElem => ripple($rippleElem));
+
+
+/**
+ * Navbar toggle for mobile screen
+ */
+
+const /**{NodeList} */ $navTogglers = document.querySelectorAll("[data-nav-toggler]");  
+const /**{NodeElement} */ $navbar = document.querySelector("[data-navigation]");
+const /**{NodeElement}  */ $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navTogglers, "click", function () {
+    $navbar.classList.toggle("show");
+    $scrim.classList.toggle("active");
+});
 
 /**
  * Filter Functionality
